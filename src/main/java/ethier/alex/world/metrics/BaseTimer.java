1f3b7a94c7a696c757e26bd4d6c148bca21d8c62
Alex Ethier <aethier@orbistechnologies.com>
2014-08-27 19:59:01 -0400

    Changed timer updates to expect nanos instead of millis.

--- src/main/java/ethier/alex/world/metrics/BaseTimer.java
+++ src/main/java/ethier/alex/world/metrics/BaseTimer.java
@@ -18,17 +18,11 @@ import org.apache.logging.log4j.Logger;
  @author alex
  */
 public class BaseTimer implements Timer {
-    
+
     private static final Logger logger = LogManager.getLogger(MetricFactory.class);
     private Map<String, Stopwatch> timers;
     private Map<String, Long> timerUpdates; // Allow updates of timers.
-    
-    //TODO: Re-implement stopwatch using system nano time.
-    // Add times to multimap, calculate sum on read.
-    // Consider a map implementation that is a fast write, read once implementation.
-    //   i.e. writes in data, but on a read, it will convert everything to a cached hashmap, then output values.
-    //   further writes will put it back into a 'dirty' state.
-    
+
     public BaseTimer() {
         timers = new HashMap<String, Stopwatch>();
         timerUpdates = new HashMap<String, Long>();
@@ -36,85 +30,89 @@ public class BaseTimer implements Timer {
 
     @Override
     public void continueTimer(String timerName) {
-        if(!timers.containsKey(timerName)) {
+        if (!timers.containsKey(timerName)) {
             Stopwatch stopwatch = Stopwatch.createStarted();
             timers.put(timerName, stopwatch);
         }
-        
+
         Stopwatch stopwatch = timers.get(timerName);
-        if(!stopwatch.isRunning()) {
+        if (!stopwatch.isRunning()) {
             stopwatch.start();
         }
     }
-    
+
     @Override
     public void stopTimer(String timerName) {
         Stopwatch stopwatch = timers.get(timerName);
-        if(stopwatch.isRunning()) {
+        if (stopwatch.isRunning()) {
             stopwatch.stop();
         }
     }
-    
+
     @Override
     public void printAll() {
-        for(String timerName : this.getTimerNames()) {
+        for (String timerName : this.getTimerNames()) {
             this.printTimer(timerName);
         }
     }
-    
+
     @Override
     public void printTimer(String timerName) {
-           
-        long millis = this.getMillis(timerName);
-        if(millis < 1000) {
-            logger.info("{} is at: {} milliseconds.", timerName, millis);
-        } else if(millis < 60*1000) {
-            logger.info("{} is at: {} seconds.", timerName, (millis / 1000));
-        } else if(millis < 60*60*1000) {
-            logger.info("{} is at: {} minutes.", timerName, (millis / (1000*60)));
+
+        long nanos = this.getNanos(timerName);
+        if (nanos < 1000) {
+            logger.info("{} is at: {} nanoseconds.", timerName, nanos);
+        } else if (nanos < 1000 * 1000) {
+            logger.info("{} is at: {} microseconds.", timerName, nanos / (1000));
+        } else if (nanos < 1000 * 1000 * 1000) {
+            logger.info("{} is at: {} milliseconds.", timerName, nanos / (1000 * 1000));
+        } else if (nanos < 60 * 1000) {
+            logger.info("{} is at: {} seconds.", timerName, (nanos / (1000 * 1000 * 1000)));
+        } else if (nanos < 60 * 60 * 1000) {
+            logger.info("{} is at: {} minutes.", timerName, (nanos / (1000 * 60 * 1000 * 1000)));
         } else {
-            logger.info("{} is at: {} hours.", timerName, (millis / (1000*60*60)));
+            logger.info("{} is at: {} hours.", timerName, (nanos / (1000 * 60 * 60 * 1000 * 1000)));
         }
     }
-    
+
     @Override
-    public void updateTimer(String timerName, long millis, TimeUnit timeUnit) {
-        
-        if(timeUnit != TimeUnit.MILLISECONDS) {
-            // For now, whenever we read we except millis, so only allow us to write millis.
+    public void updateTimer(String timerName, long nanos, TimeUnit timeUnit) {
+
+        if (timeUnit != TimeUnit.NANOSECONDS) {
+            // For now, whenever we read we expect nanos, so only allow us to write nanos.
             // TODO: Support reading in other time units so we can write any time unit.
-            throw new RuntimeException("updateTimer for non millisecond units not supported at this time.");
+            throw new RuntimeException("Currently only nano second updates supported for timers.");
         }
-        
+
         long timerUpdate = 0L;
-        if(timerUpdates.containsKey(timerName)) {
+        if (timerUpdates.containsKey(timerName)) {
             timerUpdate = timerUpdates.get(timerName);
         }
-        
-        timerUpdates.put(timerName, timerUpdate + millis);
+
+        timerUpdates.put(timerName, timerUpdate + nanos);
     }
-    
-    private long getMillis(String timerName) {
-        long millis = 0L;
-        if(timers.containsKey(timerName)) {
+
+    private long getNanos(String timerName) {
+        long nanos = 0L;
+        if (timers.containsKey(timerName)) {
             Stopwatch stopwatch = timers.get(timerName);
-            millis += stopwatch.elapsed(TimeUnit.MILLISECONDS);
+            nanos += stopwatch.elapsed(TimeUnit.NANOSECONDS);
         }
-        
-        if(timerUpdates.containsKey(timerName)) {
-            millis += timerUpdates.get(timerName);
+
+        if (timerUpdates.containsKey(timerName)) {
+            nanos += timerUpdates.get(timerName);
         }
-        
-        return millis;
+
+        return nanos;
     }
-    
+
     private Set<String> getTimerNames() {
         Set<String> timerNames = new HashSet<String>();
         timerNames.addAll(timers.keySet());
         timerNames.addAll(timerUpdates.keySet());
         return timerNames;
     }
-    
+
     @Override
     public void clearTimers() {
         timers.clear();
